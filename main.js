fa285f61614c23edda58d450f68e483a7f4afc0c
Gioia Ascari <gioia.ascari96@gmail.com>
2022-04-04 16:03:09 +0200

    prova

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log("Ok Vue Js :)");
+/*
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+ */
+const app = new Vue({
+  el: "#id",
+  data: {
+    mailRandom: [],
+  },
+  mounted() {
+    for (let i = 1; i <= 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          this.i = response.data;
+          this.mailRandom.push(response.data);
+          console.log(i);
+        });
+    }
+  },
+});
